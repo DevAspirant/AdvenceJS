b2fe74c0db201fb4fd8f7a31cce873d5a832080f
Falmban Ammar <devAspirant@gmail.com>
2019-03-19 15:00:27 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 
+/* ---- understand let & constant */
+// constant def into ES
 const player = "ammar";
-
+// variable def into ES
 let experiance = 100;
-
+// variable def into ES
 let wizard = false;
 
 if (experiance > 90) {
@@ -11,3 +13,13 @@ if (experiance > 90) {
   console.log("inside: " + wizard);
 }
 console.log("outside: " + wizard);
+
+/* constant object : you can change the constant by assign the proprties */
+const obj = {
+  player: "bayan",
+  experiance: 1,
+  wizard: false
+};
+obj.wizard = true;
+let check = obj.wizard;
+console.log(check);
